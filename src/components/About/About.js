b51da5d93e4ee71c0b29dcd8321f5418b8e9d638
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-04-27 17:48:50 +0100

    Split up main scss file into partials.
    Split main scss file, started to construct layout of About.js.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,33 +1,42 @@
 const About = () => {
   return (
     <section className="about-section">
-      <h3>About</h3>
-      <form className="about-form">
-        <div>
-          <label htmlFor="name" className="name-label">
-            Name:
-            <input type="text" id="name" name="name" className="input-name" />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="name" className="email-label">
-            E-mail:
-            <input
-              type="text"
-              id="email"
-              name="email"
-              className="input-email"
-            />
-          </label>
-        </div>
-        <div className="message-container">
-          <label htmlFor="message" className="message-label">
-            Message Me:
-            <textarea name="message-content" className="message" rows={10} cols={40}/>
-          </label>
-          <br />
-        </div>
-      </form>
+      <div className="about-heading-container">
+        <h3>About</h3>
+      </div>
+      <div className="form-container">
+        <form className="about-form">
+          <div>
+            <label htmlFor="name" className="name-label">
+              Name:
+              <input type="text" id="name" name="name" className="input-name" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="name" className="email-label">
+              E-mail:
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="input-email"
+              />
+            </label>
+          </div>
+          <div className="message-container">
+            <label htmlFor="message" className="message-label">
+              Message:
+              <textarea
+                name="message-content"
+                className="message"
+                rows={10}
+                cols={40}
+              />
+            </label>
+            <br />
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
