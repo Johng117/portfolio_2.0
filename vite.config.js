d912b38a3e6f3e967bd5d30a99b2db7720f5968e
Johng117 <johngorman117@gmail.com>
2024-03-18 20:19:52 +0000

    updates to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import svgr from "vite-plugin-svgr";
-
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
     },
-    plugins: [react(), eslint(), svgr()],
+    plugins: [react(), eslint() ],
     // server: {
     //   watch: {
     //     usePolling: true,
